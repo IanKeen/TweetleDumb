bb435f7ade0e9e096f2060680ed6f552004dfad9
Ian Keen <ian.keen@metalab.co>
2017-06-13 00:35:37 -0700

    Add User model

---
+++ TweetleDumb/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  TweetleDumb
+//
+//  Created by Ian Keen on 2017-06-12.
+//  Copyright © 2017 Mustard. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let realName: String
+    let email: String
+    let handle: String
+}
