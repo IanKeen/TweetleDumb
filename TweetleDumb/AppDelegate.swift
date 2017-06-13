389015fc47a0373030a57880518ec21a14cc94e6
Ian Keen <ian.keen@metalab.co>
2017-06-12 20:07:33 -0700

    Create initial project

---
+++ TweetleDumb/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TweetleDumb
+//
+//  Created by Ian Keen on 2017-06-12.
+//  Copyright © 2017 Mustard. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
