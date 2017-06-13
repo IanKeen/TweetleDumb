49b3f718058a224313c36b7c09f36e8652f90581
Ian Keen <ian.keen@metalab.co>
2017-06-12 22:09:35 -0700

    Add Collection random element extension

---
+++ TweetleDumb/Utilities/Collection+Random.swift
@@ -0,0 +1,18 @@
+//
+//  Collection+Random.swift
+//  TweetleDumb
+//
+//  Created by Ian Keen on 2017-06-12.
+//  Copyright © 2017 Mustard. All rights reserved.
+//
+
+import Foundation
+
+extension Collection where IndexDistance == Int, Index == Int {
+    func random() -> Iterator.Element? {
+        guard !isEmpty else { return nil }
+
+        let index = Int(arc4random() % UInt32(count))
+        return self[index]
+    }
+}
