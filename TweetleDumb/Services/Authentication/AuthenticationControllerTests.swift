5b89432628b0cffbffe46e7cdf974d53c8abfd68
Ian Keen <ian.keen@metalab.co>
2017-06-13 17:15:50 -0700

    Add AuthenticationController tests

---
+++ TweetleDumb/Services/Authentication/AuthenticationControllerTests.swift
@@ -0,0 +1,188 @@
+//
+//  AuthenticationControllerTests.swift
+//  TweetleDumb
+//
+//  Created by Ian Keen on 2017-06-13.
+//  Copyright © 2017 Mustard. All rights reserved.
+//
+
+import XCTest
+@testable import TweetleDumb
+
+private final class Delegate: AuthenticationControllerDelegate {
+    typealias OnError = (Error) -> Void
+    typealias OnLogin = (Authentication) -> Void
+    typealias OnLogout = () -> Void
+
+    private let onError: OnError
+    private let onLogin: OnLogin
+    private let onLogout: OnLogout
+
+    init(onError: @escaping OnError, onLogin: @escaping OnLogin, onLogout: @escaping OnLogout) {
+        self.onError = onError
+        self.onLogin = onLogin
+        self.onLogout = onLogout
+    }
+
+    func authenticationError(controller: AuthenticationController, error: Error) {
+        onError(error)
+    }
+    func authenticationLogin(controller: AuthenticationController, authentication: Authentication) {
+        onLogin(authentication)
+    }
+    func authenticationLogout(controller: AuthenticationController) {
+        onLogout()
+    }
+}
+
+class AuthenticationControllerTests: XCTestCase {
+    func testAuthenticationController_Initial_Logout() {
+        let exp = expectation(description: "")
+
+        let delegate = Delegate(
+            onError: { _ in XCTFail() },
+            onLogin: { _ in XCTFail() },
+            onLogout: { exp.fulfill() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: NSMutableDictionary(),
+            delegate: delegate,
+            authenticators: []
+        )
+        controller.notifyDelegate()
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testAuthenticationController_Initial_Login() {
+        let exp = expectation(description: "")
+
+        let delegate = Delegate(
+            onError: { _ in XCTFail() },
+            onLogin: { _ in exp.fulfill() },
+            onLogout: { XCTFail() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: authenticationStorage(),
+            delegate: delegate,
+            authenticators: []
+        )
+        controller.notifyDelegate()
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+
+    func testAuthenticationController_Error_UnsupportedAuth() {
+        let exp = expectation(description: "")
+
+        let delegate = Delegate(
+            onError: { exp.fulfill(when: $0, equals: AuthenticationController.Error.unsupportedAuthenticator) },
+            onLogin: { _ in XCTFail() },
+            onLogout: { XCTFail() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: NSMutableDictionary(),
+            delegate: delegate,
+            authenticators: []
+        )
+        controller.login(using: MockTwitterAuthenticator.self)
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testAuthenticationController_Error_LoggedIn() {
+        let exp = expectation(description: "")
+
+        let delegate = Delegate(
+            onError: { exp.fulfill(when: $0, equals: AuthenticationController.Error.alreadyLoggedIn) },
+            onLogin: { _ in XCTFail() },
+            onLogout: { XCTFail() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: authenticationStorage(),
+            delegate: delegate,
+            authenticators: []
+        )
+        controller.login(using: MockTwitterAuthenticator.self)
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testAuthenticationController_Error_LoggedOut() {
+        let exp = expectation(description: "")
+
+        let delegate = Delegate(
+            onError: { exp.fulfill(when: $0, equals: AuthenticationController.Error.alreadyLoggedOut) },
+            onLogin: { _ in XCTFail() },
+            onLogout: { XCTFail() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: NSMutableDictionary(),
+            delegate: delegate,
+            authenticators: []
+        )
+        controller.logout()
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+
+    func testAuthenticationController_Login() {
+        let exp = expectation(description: "")
+
+        let delegate = Delegate(
+            onError: { _ in XCTFail() },
+            onLogin: { _ in exp.fulfill() },
+            onLogout: { XCTFail() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: NSMutableDictionary(),
+            delegate: delegate,
+            authenticators: [MockTwitterAuthenticator()]
+        )
+        controller.login(using: MockTwitterAuthenticator.self)
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testAuthenticationController_Logout() {
+        let exp = expectation(description: "")
+
+        let delegate = Delegate(
+            onError: { _ in XCTFail() },
+            onLogin: { _ in XCTFail() },
+            onLogout: { exp.fulfill() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: authenticationStorage(),
+            delegate: delegate,
+            authenticators: [MockTwitterAuthenticator()]
+        )
+        controller.logout()
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+
+    func testAuthenticationController_Storage() {
+        let exp = expectation(description: "")
+
+        let storage = NSMutableDictionary()
+
+        let delegate = Delegate(
+            onError: { _ in XCTFail() },
+            onLogin: { _ in exp.fulfill(when: { storage.value(forKey: AuthenticationController.StorageKey.authentication) != nil }) },
+            onLogout: { XCTFail() }
+        )
+        let controller = AuthenticationController(
+            api: api(),
+            storage: storage,
+            delegate: delegate,
+            authenticators: [MockTwitterAuthenticator()]
+        )
+        controller.login(using: MockTwitterAuthenticator.self)
+
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+}
