3e3e28dcf841983c6fb30dfb53ed3c8c2d70d429
Ian Keen <ian.keen@metalab.co>
2017-06-13 18:25:24 -0700

    Add UIView constraint extensions

---
+++ TweetleDumb/Utilities/UIView+Constraints.swift
@@ -0,0 +1,34 @@
+//
+//  UIView+Constraints.swift
+//  TweetleDumb
+//
+//  Created by Ian Keen on 2017-06-13.
+//  Copyright © 2017 Mustard. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func center(in view: UIView) {
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
+extension NSLayoutDimension {
+    func align(to anchor: NSLayoutDimension, offset: CGFloat = 0.0) {
+        constraint(equalTo: anchor, multiplier: 1.0, constant: offset).isActive = true
+    }
+    func equal(to value: CGFloat) {
+        constraint(equalToConstant: value).isActive = true
+    }
+}
+extension NSLayoutXAxisAnchor {
+    func align(to anchor: NSLayoutXAxisAnchor, offset: CGFloat = 0.0) {
+        constraint(equalTo: anchor, constant: offset).isActive = true
+    }
+}
+extension NSLayoutYAxisAnchor {
+    func align(to anchor: NSLayoutYAxisAnchor, offset: CGFloat = 0.0) {
+        constraint(equalTo: anchor, constant: offset).isActive = true
+    }
+}
