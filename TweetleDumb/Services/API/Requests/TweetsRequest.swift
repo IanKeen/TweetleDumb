77048dae9251202c67a3e31a59ccf3d0b44279df
Ian Keen <ian.keen@metalab.co>
2017-06-13 23:03:10 -0700

    Add TweetsRequest and mock data

---
+++ TweetleDumb/Services/API/Requests/TweetsRequest.swift
@@ -0,0 +1,28 @@
+//
+//  TweetsRequest.swift
+//  TweetleDumb
+//
+//  Created by Ian Keen on 2017-06-13.
+//  Copyright © 2017 Mustard. All rights reserved.
+//
+
+import Foundation
+
+struct TweetsRequest: APIRequest {
+    func networkRequest(baseURL: URL) throws -> NetworkRequest {
+        return NetworkRequest(
+            method: .GET,
+            url: baseURL.appendingPathComponent("/v1/tweets"),
+            headers: [:],
+            body: nil
+        )
+    }
+
+    func handle(response: NetworkResponse) throws -> [Tweet] {
+        guard
+            let json = response.jsonArray as? [[String: Any]]
+            else { throw NetworkError.invalidResponse(response) }
+
+        return try json.flatMap(Tweet.init(json:))
+    }
+}
