49539fdee17726a7b2e9736cf61f266363973ff5
Ian Keen <ian.keen@metalab.co>
2017-06-13 23:50:48 -0700

    Update Tweets screen to show Tweet cells

--- TweetleDumb/ViewControllers/Tweets/TweetsViewController.swift
+++ TweetleDumb/ViewControllers/Tweets/TweetsViewController.swift
@@ -8,19 +8,30 @@
 
 import UIKit
 
-final class TweetsViewController: CustomViewController<TweetsView, TweetsViewModel> {
+final class TweetsViewController: CustomViewController<TweetsView, TweetsViewModel>, UITableViewDataSource {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Tweets"
 
         customView.refreshControl?.addTarget(self, action: #selector(refreshControlValueChanged), for: .valueChanged)
+        customView.register(cells: viewModel.cells)
+        customView.dataSource = self
 
         viewModel.delegate.add(self)
+        customView.refreshControl?.beginRefreshing()
         viewModel.reloadData()
     }
     deinit {
         viewModel.delegate.remove(self)
+    }
+
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.dataSet.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return viewModel.dataSet[indexPath.row].dequeue(from: tableView, at: indexPath)
     }
 
     // MARK: - Actions
@@ -31,9 +42,10 @@ final class TweetsViewController: CustomViewController<TweetsView, TweetsViewMod
 
 extension TweetsViewController: TweetsViewModelDelegate {
     func tweetsViewModel(_ viewModel: TweetsViewModel, error: Error) {
-        //
+        customView.refreshControl?.endRefreshing()
     }
     func tweetsViewModelUpdated(_ viewModel: TweetsViewModel) {
         customView.reloadData()
+        customView.refreshControl?.endRefreshing()
     }
 }
--- TweetleDumb/ViewControllers/Tweets/TweetsViewModel.swift
+++ TweetleDumb/ViewControllers/Tweets/TweetsViewModel.swift
@@ -18,9 +18,16 @@ final class TweetsViewModel {
 
     // MARK: - Private Properties
     private let dependencies: Dependencies
+    private var working = false
 
     // MARK: - Public Properties
     let delegate = MulticastDelegate<TweetsViewModelDelegate>()
+    let cells: [TableViewCellViewModel.Type] = [TweetCellViewModel.self]
+    private(set) var dataSet: [TableViewCellViewModel] = [] {
+        didSet {
+            delegate.notify { $0.tweetsViewModelUpdated(self) }
+        }
+    }
 
     // MARK: - Lifecycle
     init(dependencies: Dependencies) {
@@ -29,6 +36,25 @@ final class TweetsViewModel {
 
     // MARK: - Public Functions
     func reloadData() {
-        print("Reload Data")
+        guard !working else { return }
+
+        working = true
+        let request = TweetsRequest()
+
+        dependencies.api.execute(request: request) { [weak self] result in
+            guard let this = self else { return }
+
+            switch result {
+            case .success(let tweets):
+                this.dataSet = tweets
+                    .sorted(by: { $0.posted > $1.posted })
+                    .map(TweetCellViewModel.init)
+
+            case .failure(let error):
+                this.delegate.notify { $0.tweetsViewModel(this, error: error) }
+            }
+
+            this.working = false
+        }
     }
 }
