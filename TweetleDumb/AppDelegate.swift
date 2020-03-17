65ddd3bafb4c0557fdc19abbc61dba2cbd167930
Yoel Jimenez del Valle <yoel07@gmail.com>
2020-03-17 11:41:22 -0400

    swift 5 update

--- TweetleDumb/AppDelegate.swift
+++ TweetleDumb/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var applicationCoordinator: ApplicationCoordinator!
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let navigationController = UINavigationController()
 
